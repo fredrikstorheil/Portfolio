efe82b7612f13e107718daa742a4f4d4283debb4
fredrikstorheil <fredrikstorheil@gmail.com>
2025-04-10 15:25:21 +0200

    Refactor schema structure by removing old schemas and adding new document types for case study, homepage, about, and contact

--- ux-portfolio-studio/sanity.cli.ts
+++ ux-portfolio-studio/sanity.cli.ts
@@ -5,6 +5,7 @@ export default defineCliConfig({
     projectId: '8njc2y4b',
     dataset: 'production'
   },
+  studioHost: 'ux-portfolio',
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
